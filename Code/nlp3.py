1138f00a69aee0eb316ee9d7ad34a1d9455ddaef
Neha Yadav <mailneha17@gmail.com>
2017-05-15 15:48:46 -0400

    Update nlp3.py

--- Code/nlp3.py
+++ Code/nlp3.py
@@ -13,7 +13,7 @@ def main(start, stop):
 	start_time = time.time()
 	print 'start stop = ', start, stop
 
-	stanford_dir = 'C:\Users\Arunima Chaudhary\Desktop\NLP Project\stanford-postagger-full-2015-04-20\\'
+	stanford_dir = '.\data\stanford-postagger-full-2015-04-20\\'
 	modelfile = stanford_dir + 'models\english-bidirectional-distsim.tagger'
 	jarfile = stanford_dir + 'stanford-postagger.jar'
 
@@ -29,7 +29,7 @@ def main(start, stop):
 	real_matches = 0
 	correct_matches = 0
 	wrong_matches = 0
-	with open("C:\Users\Arunima Chaudhary\Downloads\quora_duplicate_questions.tsv") as tsvfile:
+	with open(".\data\quora_duplicate_questions.tsv") as tsvfile:
 	    tsvreader = csv.reader(tsvfile, delimiter="\t")
 	    #print 'inside thread call....start = ',start,"\tstop = ",stop
 	    #print 'inside run = ', (stop/100)
@@ -142,80 +142,7 @@ def main(start, stop):
 
 
 
-try:
-
-	# stanford_dir = 'C:\Users\Arunima Chaudhary\Desktop\NLP Project\stanford-postagger-full-2015-04-20\\'
-	# modelfile = stanford_dir + 'models\english-bidirectional-distsim.tagger'
-	# jarfile = stanford_dir + 'stanford-postagger.jar'
-
-	# tagger = StanfordPOSTagger(model_filename=modelfile, path_to_jar=jarfile)
-	# q1 = 'What is the step by step guide to invest in share market?'
-	# q2 = 'What is the step by step guide to invest in share market in india?'
-
-	# q1_tags = tagger.tag(nltk.word_tokenize(q1))
-	# q2_tags = tagger.tag(nltk.word_tokenize(q2))
-
-	# q1_nouns = dict()
-	# q1_verbs = dict()
-	# for word, tag in q1_tags:
-	# 	if tag[:2] == 'NN':
-	# 		q1_nouns[word] = 1
-	# 	if tag[:2] == 'VB':
-	# 		q1_verbs[word] = 1
-
-	# q2_nouns = dict()
-	# q2_verbs = dict()
-	# for word, tag in q1_tags:
-	# 	if tag[:2] == 'NN':
-	# 		q2_nouns[word] = 1
-	# 	if tag[:2] == 'VB':
-	# 		q2_verbs[word] = 1
-
-	# print q1_nouns
-	# print q1_verbs
-	# print q2_nouns
-	# print q2_verbs
-
+try:	
 	main(2, 21)
-
-# 	start_num = []
-# 	stop_num = []
-# #2425743
-# 	for i in range(0, 2426):
-# 		if i == 0:
-# 			start_num.append(2)
-# 		else:
-# 			start_num.append(i*1000)
-
-# 		if i == 2425:
-# 			stop_num.append(2425743)
-# 		else:
-# 			stop_num.append((i+1)*1000)
-
-# 	print start_num
-# 	print stop_num
-# 	# start_num = [2,101,201,301,401,501,61,701,801,901]
-# 	# stop_num = [100,200,300,400,500,600,700,800,900,1000]
-
-# 	thread_list = []
-
-# 	wn.ensure_loaded() 
-
-# 	for i in range(0, 2426):
-# 	    # Instantiates the thread
-# 	    t = threading.Thread(target=main, args=(start_num[i],stop_num[i],))
-# 	    # Sticks the thread in a list so that it remains accessible
-# 	    print 'thread ',i
-# 	    thread_list.append(t)
-
-# 	for thread in thread_list:
-# 		print 'starting thread'
-# 		thread.start()
-
-# 	for thread in thread_list:
-# 		thread.join()
-
-# 	print 'Done'
-
 except:
    print "Error: unable to start thread"
